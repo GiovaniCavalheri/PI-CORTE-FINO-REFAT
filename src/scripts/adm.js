0161a680ea21fde42eefc05d74658d3bcfb41e88
Giovani Cavalheri <inocenciogiovanin@gmail.com>
2026-03-29 22:41:05 -0300

    finish-project

--- src/scripts/adm.js
+++ src/scripts/adm.js
@@ -2,7 +2,7 @@ document.querySelector(".form-login").addEventListener("submit", function (e) {
   e.preventDefault();
   if (document.getElementById("senha").value === "root123") {
     alert("Login feito!");
-    window.location.href = "/index.html";
+    window.location.href = "../Interface/pag-adm.html";
   } else {
     alert("Erro! Senha: root123");
   }
